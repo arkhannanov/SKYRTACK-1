9e2b53bcf925f9636c8da0955435887335a0fecd
arkhannanov <arkhannanov@gmail.com>
2019-05-27 11:49:54 +0300

    #3 Refactoring.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import './js/favicons.js'
 import './scss/main.scss'
 
-// const scssContext = require.context(
-//     '!!file-loader?name=./blocks/avatar/[name].[ext]!.',
-//     true,
-//     /\.(scss)$/
-//   );
-
 function importAll(resolve) {
     resolve.keys().forEach(resolve);
   }
